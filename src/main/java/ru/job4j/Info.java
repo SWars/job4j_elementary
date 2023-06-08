908b801bb04aa61dac5efbdc0b3963d5f3511dfa
ceh9i <free-teen2009@yandex.ru>
2023-06-08 21:16:17 +0700

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,7 +1,7 @@
-package ru.job4j.Info;
+package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("08.06.2022");
+        System.out.println("08.06.2023");
     }
 }
