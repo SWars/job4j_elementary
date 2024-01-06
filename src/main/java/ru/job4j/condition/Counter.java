e7dcc9e3140025eb4748e40b6264de6a5f09696b
raleshkin <free-teen2009@yandex.ru>
2024-01-06 15:30:12 +0700

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/condition/Counter.java
+++ src/main/java/ru/job4j/condition/Counter.java
@@ -9,10 +9,25 @@ public class Counter {
         return sum;
     }
 
+    public static int sumByEvent(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
         System.out.println(sum(0, 5));
+        System.out.println();
+        System.out.println(sumByEvent(0, 10));
+        System.out.println(sumByEvent(3, 8));
+        System.out.println(sumByEvent(1, 1));
+        System.out.println(sumByEvent(0, 5));
     }
 }
--- src/test/java/ru/job4j/condition/CounterTest.java
+++ src/test/java/ru/job4j/condition/CounterTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.*;
 class CounterTest {
 
     @Test
-    void whenStart0Finish5Then15() {
+    void whenStart0Finish5ThenSum15() {
         int start = 0;
         int finish = 5;
         int result = Counter.sum(start, finish);
@@ -16,7 +16,7 @@ class CounterTest {
     }
 
     @Test
-    void whenStart10Finish5Then0() {
+    void whenStart10Finish5ThenSum0() {
         int start = 10;
         int finish = 5;
         int result = Counter.sum(start, finish);
@@ -25,11 +25,48 @@ class CounterTest {
     }
 
     @Test
-    void whenStart1Finish1Then1() {
+    void whenStart1Finish1ThenSum1() {
         int start = 1;
         int finish = 1;
         int result = Counter.sum(start, finish);
         int expected = 1;
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStart0Finish5ThenSumByEvent6() {
+        int start = 0;
+        int finish = 5;
+        int result = Counter.sum(start, finish);
+        int expected = 15;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStart10Finish5ThenSumByEvent0() {
+        int start = 10;
+        int finish = 5;
+        int result = Counter.sum(start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStart1Finish1ThenSumByEvent0() {
+        int start = 1;
+        int finish = 1;
+        int result = Counter.sum(start, finish);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStartMinus3Finish5ThenSumByEvent4() {
+        int start = -3;
+        int finish = 5;
+        int result = Counter.sumByEvent(start, finish);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+
     }
 }
