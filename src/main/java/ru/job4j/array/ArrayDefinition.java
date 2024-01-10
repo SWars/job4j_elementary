01c63c1cae14a12597efa261e9109cd14b6caf7a
raleshkin <free-teen2009@yandex.ru>
2024-01-10 13:07:01 +0700

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,13 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surname.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Роман Алешкин";
+        names[1] = "Семён Князев";
+        names[2] = "Тимур Головня";
+        names[3] = "Антон Легалов";
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
